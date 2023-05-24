ee6a2feb9cc1d4cfe89a35a8d6d2cc393312427e
Jose Guambo <jlguambo@gmail.com>
2023-05-24 00:59:39 -0500

    feat:Add scroll controls and model.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,68 @@
 import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useLoader, useThree } from '@react-three/fiber'
 import Box from './components/Box';
-import { OrbitControls, OrthographicCamera, Scroll, ScrollControls } from '@react-three/drei';
+import { Environment, OrbitControls, OrthographicCamera, Scroll, ScrollControls, Sky, useTexture } from '@react-three/drei';
 import Scene from './components/Scene';
+import Cube from './components/Cube';
+import Scene2 from './components/Scene2';
+import Scene3 from './components/Scene3';
+import { Model } from './Duck';
+import SceneModel from './components/SceneModel';
+import { TextureLoader } from 'three';
+import galaxyImage from './galaxy.jpg'
+import SkyBox from './components/Skybox';
+
 
 function App() {
+ 
+
   return (
-    <div className="container">
-    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, -100, 200], fov: 100 }}>
+    <div className="container"  >
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [600, 600, 600], fov: 100 }}
+  
+    >
+     
+    {/* <Environment
+        files="https://cdn.jsdelivr.net/gh/Sean-Bradley/React-Three-Fiber-Boilerplate@environment/public/img/venice_sunset_1k.hdr"
+        background
+        blur={0.9}
+      /> */}
       {/* <Suspense fallback={null}>
        <Scene />
       </Suspense> */}
 
-      <OrbitControls />
-
+{/* <OrbitControls /> */}
 
 <ScrollControls pages={3} damping={0.1}>
   {/* Canvas contents in here will *not* scroll, but receive useScroll! */}
-  
+ 
   <Scroll>
-    {/* Canvas contents in here will scroll along */}
-   <Scene />
+    <Scene3 />
   </Scroll>
-  <Scroll html>
-    {/* DOM contents in here will scroll along */}
-    <h1>html in here (optional)</h1>
-    <h1 style={{ top: '100vh' }}>second page</h1>
-    <h1 style={{ top: '200vh' }}>third page</h1>
+  <Scroll>
+    <Scene2 />
+  </Scroll>
+  <Scroll>
+    <Scene3 />
+  </Scroll>
+  <Scroll>
+    <Scene />
+  </Scroll>
+  <Scroll>
+   <SceneModel />
   </Scroll>
 </ScrollControls>
       <axesHelper args={[500]} />
+      <ambientLight />
+      <Sky
+             distance={450000}
+             sunPosition={[5, 1, 8]}
+             inclination={0}
+             azimuth={0.25}
+           
+         />
       </Canvas>
     </div>
   )
---
+++ src/components copy/Scene2/index.tsx
@@ -0,0 +1,36 @@
+import { useLoader } from '@react-three/fiber';
+import Box from '../Box';
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import galaxyImage from '../../galaxy.jpg'
+
+
+const Scene2 = () => {
+
+    const colorMap = useLoader(TextureLoader, galaxyImage)
+
+
+  return (
+    <>
+      <ambientLight intensity={0.25} />
+      <pointLight intensity={1} position={[1000, 1000, 1000]} />
+     
+      <mesh
+      
+      position={[0, -1000, 0]}
+      >
+        <sphereGeometry args={[300,300,300]} />
+        <meshStandardMaterial map={colorMap} />
+     
+      </mesh>
+        
+      <Box position={[100, -400, 100]} color={'purple'} />
+      <Box position={[200, -100, 100]} color={'green'} />
+      <Box position={[0, -400, -200]} color={'red'} />
+      {/* <Box position={[-100, 100, 0]} color={'yellow'} />
+      <Box position={[100, -100, 0]}  color={'black'}/>
+      <Box position={[-100, -100, 0]}  color={'brown'}/> */}
+      </>
+  )
+}
+
+export default Scene2;
